d7cce8151e062885413b52f4ac436a5bbb7a393d
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-22 01:07:10 +0600

    add login and signup pages ui

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
+      <div className='flex flex-col w-full max-w-md shadow-lg p-8'>
+        <h2 className='text-center text-4xl uppercase mb-10'>Login</h2>
+
+        <button className='btn btn-outline uppercase mb-2'>
+          Continue with Google
+        </button>
+
+        <div className='divider my-5'>OR</div>
+
+        <form>
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-base'>Email</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Email'
+              className='input input-bordered w-full'
+            />
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-base'>Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Password'
+              className='input input-bordered w-full'
+            />
+            <label className='label'>
+              <span className='label-text-alt'>Forget Password?</span>
+            </label>
+          </div>
+
+          <input
+            type='submit'
+            value='Login'
+            className='btn btn-secondary w-full mt-5 mb-2'
+          />
+        </form>
+
+        <p className='text-center text-sm'>
+          New to RollaBike?{' '}
+          <Link to='/signup' className='text-blue-400'>
+            Sign Up Now
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
-  return <div>Sign UP</div>;
+  return (
+    <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
+      <div className='flex flex-col w-full max-w-md shadow-lg p-8'>
+        <h2 className='text-center text-4xl uppercase mb-10'>Sign Up</h2>
+
+        <button className='btn btn-outline uppercase mb-2'>
+          Continue with Google
+        </button>
+
+        <div className='divider my-5'>OR</div>
+
+        <form>
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-base'>Full Name</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Full Name'
+              className='input input-bordered w-full'
+            />
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-base'>Email</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Email'
+              className='input input-bordered w-full'
+            />
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-base'>Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Password'
+              className='input input-bordered w-full'
+            />
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-base'>Confirm Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              className='input input-bordered w-full'
+            />
+          </div>
+
+          <input
+            type='submit'
+            value='Sign Up'
+            className='btn btn-secondary w-full mt-5 mb-2'
+          />
+        </form>
+
+        <p className='text-center text-sm'>
+          Already have an Account?{' '}
+          <Link to='/login' className='text-blue-400'>
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;

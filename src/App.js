24eda0a5fd4a85de222fa4a5c7695ad6956c1a19
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-22 00:42:41 +0600

    set up routes using demo components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import Blogs from './Pages/Blogs/Blogs';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
 import Navbar from './Pages/Shared/Navbar';
+import Signup from './Pages/Signup/Signup';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/myPortfolio' element={<MyPortfolio />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyPortfolio = () => {
+  return <div>My Portfolio</div>;
+};
+
+export default MyPortfolio;

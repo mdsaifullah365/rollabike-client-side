6d677a1c8122a2b3121fffcca87f7179cb2bcb12
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-25 04:15:38 +0600

    update product section design on the home page

--- src/Pages/Home/ProductCard.js
+++ src/Pages/Home/ProductCard.js
@@ -4,16 +4,20 @@ import { Link } from 'react-router-dom';
 const ProductCard = ({ product }) => {
   const { _id, name, image, description, minimum, available, price } = product;
   return (
-    <div className='card card-compact bg-neutral text-white shadow-xl'>
-      <figure className='h-1/2'>
+    <div class='card text-base-100 shadow-xl glass'>
+      <figure>
         <img src={image} alt={name} />
       </figure>
       <div className='card-body'>
-        <h2 className='card-title'>{name}</h2>
-        <p>{description}</p>
-        <p>Minimum Order Quantity: {minimum}</p>
-        <p>Available Quantity: {available}</p>
-        <p>Price Per Unit: ${price}</p>
+        <h2 className='card-title text-2xl mb-4'>{name}</h2>
+        <p className='text-xl my-0'>{description}</p>
+        <p className='text-xl my-0 text-primary'>
+          Minimum Order Quantity: {minimum}
+        </p>
+        <p className='text-xl my-0 text-primary'>
+          Available Quantity: {available}
+        </p>
+        <p className='text-xl my-0 text-primary'>Price Per Unit: ${price}</p>
         <div className='card-actions justify-end'>
           <Link to={`/purchase/${_id}`} className='btn btn-primary'>
             Purchase
--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -1,20 +1,22 @@
-import axios from "axios";
-import React from "react";
-import { useQuery } from "react-query";
-import Loading from "../Shared/Loading";
-import ProductCard from "./ProductCard";
+import axios from 'axios';
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import ProductCard from './ProductCard';
 
 const Products = () => {
-  const { data: products, isLoading } = useQuery("products", () =>
-    axios.get("/product")
+  const { data: products, isLoading } = useQuery('products', () =>
+    axios.get('/product')
   );
   if (isLoading) {
     return <Loading />;
   }
   return (
-    <section className="container">
-      <div className="text-4xl text-center text-dark">Motorbike Parts</div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+    <section className='container my-20'>
+      <div className='text-4xl text-center text-base-100 mb-12 uppercase'>
+        RollaBike <span className='text-primary'>Parts</span>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
         {products?.data.map((product) => (
           <ProductCard key={product._id} product={product} />
         ))}

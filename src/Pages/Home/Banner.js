2e3f0100f14e60f5a4496c2935977c651660f323
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-25 21:02:57 +0600

    add banner

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  Autoplay,
+  EffectFade,
+  Keyboard,
+  Navigation,
+  Pagination,
+  Zoom,
+} from 'swiper';
+import slide1 from '../../images/banner/bike.svg';
+import slide2 from '../../images/banner/customerSupport.svg';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import 'swiper/css/zoom';
+
+const Banner = () => {
+  return (
+    <>
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#e2e8f0',
+          '--swiper-pagination-color': '#38bdf8',
+        }}
+        navigation={true}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        effect={'fade'}
+        keyboard={{
+          enabled: true,
+        }}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        zoom={true}
+        modules={[Navigation, Pagination, EffectFade, Keyboard, Autoplay, Zoom]}
+        className='mySwiper mb-20 bg-neutral'>
+        <SwiperSlide>
+          <div className='container grid md:grid-cols-2 items-center gap-10 bg-neutral text-base-200 py-24 md:py-40'>
+            <div className='px-5'>
+              <h1 className='text-5xl mb-5 leading-snug'>
+                Motorcycle Parts At Best{' '}
+                <span className='text-primary'>Price Ever</span>
+              </h1>
+              <p className='text-xl'>
+                We ensure you to give you best motorcycle parts at best prices.
+                You can purchase unlimited products with great price ever. Get
+                access our huge stock. Grow your business with us.
+              </p>
+            </div>
+            <div>
+              <img className='block w-3/4 mx-auto' src={slide1} alt='' />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='container grid md:grid-cols-2 items-center gap-10 bg-neutral text-base-200 py-24 md:py-40'>
+            <div className='px-5'>
+              <h1 className='text-5xl mb-5 leading-snug'>
+                Connect Our Customer{' '}
+                <span className='text-primary'>Support Representative</span>
+              </h1>
+              <p className='text-xl'>
+                Are you facing any difficulties purchasing our products? Here
+                our best customer support representative is waiting for you. Get
+                any help or queries any time!
+              </p>
+            </div>
+            <div>
+              <img className='block w-3/4 mx-auto' src={slide2} alt='' />
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Banner;
